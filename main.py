9480bf15874e5b4fb0c1c654e9ec1141d8ed2a38
DanielFontalvo1 <danielfon1999@gmail.com>
2020-03-28 00:33:34 +0000

    Actualización POO

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import re
+import emoji
+
+class ValidatePassaword():
+  def __init__(self,contraseña,expresion):
+     self.contraseña=contraseña
+     self.expresion=expresion
+   
+  @staticmethod
+  def validar():
+      if re.search(expresion, contraseña):
+        print("Contraseña valida \U0001f44d \U0001F604")
+      else:
+        print("contraseña invalida \U0001F44E \U0001F641	")
+
 
 print("Ingrese Contraseña")
-Contraseña=input()
-if re.search('^[A-Z][0-9]{3}[a-z]+[\W]{3}$',Contraseña):
-  print("Contraseña Valida")
-else:
-  print("Contraseña invalida")
+contraseña=input()
+expresion='^[A-Z][0-9]{3}[a-z]+[\W]{3}$'
+validar=ValidatePassaword(contraseña,expresion)
+validar.validar()
