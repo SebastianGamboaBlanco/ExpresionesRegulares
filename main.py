f8e32346083a8cb404eb5bf6b9c7fdd683ab9aea
SebastianGamboaBlanco <sebastiangb007@gmail.com>
2020-03-28 00:46:31 +0000

    Reglas

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import re
 import emoji
-
+print("El validador de contraseña solo valida.\n ")
+print("1- Primer caracter debe ser una letra mayuscula.")
+print("2- Los siguientes 3 caracteres son números.")
+print("3- Los siguientes caracteres deben ser letras minúsculas.")
+print("4- los últimos 3 caracteres deben ser caracteres especiales.\n")
 class ValidatePassaword():
   def __init__(self,contraseña,expresion):
      self.contraseña=contraseña
@@ -9,12 +13,12 @@ class ValidatePassaword():
   @staticmethod
   def validar():
       if re.search(expresion, contraseña):
-        print("Contraseña valida \U0001f44d \U0001F604")
+        print("\nContraseña valida \U0001f44d \U0001F604\n")
       else:
-        print("contraseña invalida \U0001F44E \U0001F641	")
+        print("\ncontraseña invalida \U0001F44E \U0001F641\n")
 
 
-print("Ingrese Contraseña")
+print("Ingrese Contraseña\n")
 contraseña=input()
 expresion='^[A-Z][0-9]{3}[a-z]+[\W]{3}$'
 validar=ValidatePassaword(contraseña,expresion)
