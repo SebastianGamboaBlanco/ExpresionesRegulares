5f9cf720e871bc7f4c4b3f862dc990a5d17b4183
SebastianGamboaBlanco <sebastiangb007@gmail.com>
2020-03-28 01:59:11 +0000

    Detalles finales

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import re
 import emoji
-print("El validador de contraseña solo valida.\n ")
-print("1- Primer caracter debe ser una letra mayuscula.")
-print("2- Los siguientes 3 caracteres son números.")
-print("3- Los siguientes caracteres deben ser letras minúsculas.")
-print("4- los últimos 3 caracteres deben ser caracteres especiales.\n")
+import os
+def Reglas():
+    print("El validador de contraseña solo valida.\n ")
+    print("1- Primer caracter debe ser una letra mayuscula.")
+    print("2- Los siguientes 3 caracteres son números.")
+    print("3- Los siguientes caracteres deben ser letras minúsculas.")
+    print("4- los últimos 3 caracteres deben ser caracteres especiales.\n")
 class ValidatePassaword():
   def __init__(self,contraseña,expresion):
      self.contraseña=contraseña
@@ -17,9 +19,12 @@ class ValidatePassaword():
       else:
         print("\ncontraseña invalida \U0001F44E \U0001F641\n")
 
-
-print("Ingrese Contraseña\n")
-contraseña=input()
-expresion='^[A-Z][0-9]{3}[a-z]+[\W]{3}$'
-validar=ValidatePassaword(contraseña,expresion)
-validar.validar()
+while True:
+  Reglas()
+  print("Ingrese Contraseña\n")
+  contraseña=input()
+  expresion='^[A-Z][0-9]{3}[a-z]+[\W]{3}$'
+  validar=ValidatePassaword(contraseña,expresion)
+  validar.validar()
+  input("oprima una ENTER para continuar")
+  os.system('clear')
